2cb1e305bcfa18f843dee52c8df5f2094f980884
Jason <jason@zgwit.com>
2021-07-31 18:12:47 +0800

    设备管理页面，未完成

--- src/app/main/device-edit/device-edit.component.ts
+++ src/app/main/device-edit/device-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormArray, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {TabRef} from "../../helper/tabs/tabs.component";
+import {ActivatedRoute} from "@angular/router";
+import {RequestService} from "../../request.service";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 @Component({
   selector: 'app-device-edit',
@@ -6,10 +11,104 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device-edit.component.scss']
 })
 export class DeviceEditComponent implements OnInit {
+  id: any;
+  submitting = false;
 
-  constructor() { }
+  basicForm: FormGroup = new FormGroup({});
+  jobsArray: FormArray = new FormArray([]);
+
+  data: any = {
+    "name": "新建设备",
+    "tunnel_id": "",
+    "slave": 1843,
+    "jobs": []
+  }
+
+  constructor(private fb: FormBuilder, private tab: TabRef, private route: ActivatedRoute, private rs: RequestService, private message: NzMessageService) {
+    this.id = route.snapshot.paramMap.get('id');
+    tab.name = this.id ? "编辑设备" : "新建设备";
+    if (this.id) this.load();
+    this.buildForm();
+  }
+
+  buildForm(): void {
+    this.basicForm = this.fb.group({
+      name: [this.data.name, [Validators.required]],
+      tunnel_id: [this.data.address, [Validators.required]],
+      slave: [this.data.port, [Validators.required]],
+
+      jobs: this.jobsArray = this.fb.array(this.data.jobs.map((d: any) => {
+        return {
+          crontab: [d.crontab, [Validators.required]], //应该是最大值+1
+          commands: [d.commands, [Validators.required]],
+        }
+      })),
+    });
+  }
 
   ngOnInit(): void {
   }
 
+
+  load(): void {
+    this.rs.get('device/' + this.id + '/detail').subscribe(res => {
+      this.data = res.data;
+      this.buildForm();
+    })
+  }
+
+  submit(): void {
+    this.submitting = true
+    const uri = this.id ? 'device/' + this.id + '/setting' : 'device/create';
+    this.rs.post(uri, this.data).subscribe(res => {
+      this.message.success("提交成功");
+      this.tab.Close();
+    }).add(() => {
+      this.submitting = false;
+    })
+  }
+
+  change() {
+    //console.log('change', e)
+    this.data = this.basicForm.value;
+  }
+
+  jobAdd() {
+    this.jobsArray.push(this.fb.group({
+      crontab: [this.jobsArray.controls.length + 1, [Validators.required]], //应该是最大值+1
+      commands: ['', [Validators.required]],
+    }))
+    //复制controls，让表格可以刷新
+    this.jobsArray.controls = [...this.jobsArray.controls];
+  }
+
+  deviceMoveUp(i: number) {
+    if (i > 0) {
+      const c = this.jobsArray.at(i);
+      this.jobsArray.removeAt(i);
+      this.jobsArray.insert(i - 1, c);
+    }
+  }
+
+  deviceMoveDown(i: number) {
+    if (i < this.jobsArray.length - 1) {
+      const c = this.jobsArray.at(i);
+      this.jobsArray.removeAt(i);
+      this.jobsArray.insert(i + 1, c);
+    }
+  }
+
+  deviceRemove(i: number) {
+    this.jobsArray.removeAt(i)
+  }
+
+  jobClear() {
+    this.jobsArray.clear();
+  }
+
+  jobSort() {
+    this.jobsArray.controls.sort((a, b) => {
+      return a.value.slave - b.value.slave;
+    })
+  }
 }
--- src/app/main/device/device.component.ts
+++ src/app/main/device/device.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewContainerRef} from '@angular/core';
+import {NzTableQueryParams} from "ng-zorro-antd/table";
+import {TabRef} from "../../helper/tabs/tabs.component";
+import {NzModalService} from "ng-zorro-antd/modal";
+import {DeviceEditComponent} from "../device-edit/device-edit.component";
+import {Router} from "@angular/router";
+import {RequestService} from "../../request.service";
 
 @Component({
   selector: 'app-device',
@@ -6,10 +12,64 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./device.component.scss']
 })
 export class DeviceComponent implements OnInit {
+  datum: any[] = [];
 
-  constructor() { }
+  loading = false;
+  total = 1;
+  pageSize = 20;
+  pageIndex = 1;
+  filterGender = [
+    {text: 'TCP服务器', value: 'tcp-server'},
+    {text: 'TCP客户端', value: 'tcp-client'}
+  ];
 
-  ngOnInit(): void {
+  params: any = {};
+
+  constructor(private tab: TabRef, private ms: NzModalService, private vcf: ViewContainerRef, private router: Router, private rs: RequestService) {
+    tab.name = "设备"
   }
 
+  ngOnInit(): void {
+    //this.load();
+  }
+
+  search(keyword: string) {
+    this.params.keyword = keyword;
+    this.load();
+  }
+
+  onQuery(params: NzTableQueryParams) {
+    this.params = params;
+    this.load();
+  }
+
+  load(): void {
+    this.loading = true;
+    this.rs.post('device/list', this.params).subscribe(res => {
+      console.log('res', res);
+      this.datum = res.data;
+      this.total = res.total;
+    }).add(() => {
+      this.loading = false;
+    });
+  }
+
+  create(): void {
+    this.router.navigate(["admin/device/create"]);
+
+    return;
+  }
+
+
+  enable(i: number) {
+
+  }
+
+  disable(i: number) {
+
+  }
+
+  remove(i: number) {
+
+  }
 }
