27ae3ca0b727a5b0efe4c0f24dd710cf64b18dfe
Jason <jason@zgwit.com>
2021-08-02 01:30:59 +0800

    项目详情

--- src/app/main/project-detail/project-detail.component.ts
+++ src/app/main/project-detail/project-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TabRef} from "../../helper/tabs/tabs.component";
+import {ActivatedRoute} from "@angular/router";
+import {RequestService} from "../../request.service";
 
 @Component({
   selector: 'app-project-detail',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-detail.component.scss']
 })
 export class ProjectDetailComponent implements OnInit {
+  id: any = '';
+  data: any = {};
 
-  constructor() { }
+  constructor(private tab: TabRef, private router: ActivatedRoute, private rs: RequestService) {
+    tab.name = '项目详情';
+    this.id = router.snapshot.params.id;
+    this.load();
+  }
 
   ngOnInit(): void {
+  }
+
+  load(): void {
+    this.rs.get(`project/${this.id}/detail`).subscribe(res=>{
+      this.data = res.data;
+    });
   }
 
 }
