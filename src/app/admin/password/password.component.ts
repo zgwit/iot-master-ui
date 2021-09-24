ea2a950d98495d1ab40d0233948de8399ab366a9
Jason <jason@zgwit.com>
2021-09-24 19:04:59 +0800

    修改密码的功能

--- src/app/admin/password/password.component.ts
+++ src/app/admin/password/password.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {TabRef} from "../../helper/tabs/tabs.component";
+import {ActivatedRoute} from "@angular/router";
+import {RequestService} from "../../request.service";
+import {AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators} from "@angular/forms";
+import {NzMessageService} from "ng-zorro-antd/message";
 
 @Component({
   selector: 'app-password',
@@ -7,9 +12,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordComponent implements OnInit {
 
-  constructor() { }
+  basicForm: FormGroup = new FormGroup({
+    old: new FormControl("", [Validators.required]),
+    new: new FormControl("", [Validators.required, Validators.minLength(6), Validators.maxLength(20)]),
+    new2: new FormControl("", [Validators.required, (control: AbstractControl): ValidationErrors | null => {
+      if (!this.basicForm) return null;
+      const ret = this.basicForm.value.new !== control.value;
+      return ret ? {diff: {value: control.value}} : null;
+    }]),
+  });
+
+  constructor(private fb: FormBuilder, private tab: TabRef, private router: ActivatedRoute, private rs: RequestService, private ms: NzMessageService) {
+    tab.name = "修改密码";
+
+  }
 
   ngOnInit(): void {
+  }
+
+  submit(): void {
+    //const val = this.basicForm.value;
+    if(!this.basicForm.valid) return;
+    this.rs.post('my/password', this.basicForm.value).subscribe(res=>{
+      this.ms.success("修改成功");
+    })
   }
 
 }
