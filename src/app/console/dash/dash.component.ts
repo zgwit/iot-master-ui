217d0b514af30d355256c4efa6b9bb75c2ef67cb
jason <jason@zgwit.com>
2021-10-31 16:35:21 +0800

    显示背景状态，此处代码非标，需优化

--- src/app/console/dash/dash.component.ts
+++ src/app/console/dash/dash.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit } from '@angular/core';
 import { TabRef } from "../../helper/tabs/tabs.component";
 import { Router, ActivatedRoute } from "@angular/router";
 import { RequestService } from "../../request.service";
@@ -8,7 +8,7 @@ import { RequestService } from "../../request.service";
   templateUrl: './dash.component.html',
   styleUrls: ['./dash.component.scss']
 })
-export class DashComponent implements OnInit {
+export class DashComponent implements OnInit, OnDestroy {
   cid = '';
 
   projectTotal = 0;
@@ -19,6 +19,8 @@ export class DashComponent implements OnInit {
   //win = (window as any);
   doc = (window as any).document;
 
+  interval: any = null;
+
   constructor(private tab: TabRef, private router: Router, private route: ActivatedRoute, private rs: RequestService, private er: ElementRef) {
     tab.name = "控制台"
     this.cid = this.route.snapshot.parent?.params?.cid;
@@ -28,6 +30,13 @@ export class DashComponent implements OnInit {
   ngOnInit(): void {
     //this.load();
     //console.log('cid: ', this.route.snapshot.parent?.params?.cid)
+    this.interval = setInterval(()=>{
+      this.load();
+    }, 5000);
+  }
+
+  ngOnDestroy(): void{
+    clearInterval(this.interval);
   }
 
   enterFullscreen() {
